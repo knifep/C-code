50dd3227ada92a2f3e49c8ff7f521e5cac5230ec
付俊尹 <1220906602@qq.com>
2023-10-31 09:16:44 +0800

    测试，学习变量

---
+++ Project2/Project2/test.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d%d", a, b);
+	return 0;
+}*/
+int main()
+{
+	int num = 4;
+	//num变量
+	//const---常属性（将变量变成常数）
+	//用法：const int num=4;
+	//const修饰的常变量
+	printf("%d%d", num);
+	return 0;
+}
+/*
+int arr[10]={0}
+数组创建方式*/
